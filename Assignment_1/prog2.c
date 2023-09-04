e18bf59620b9d12ada5f36405cf2508dbdb4e58f
Brythe40 <113539993+Brythe40@users.noreply.github.com>
2023-09-04 11:57:08 -0500

    Update prog2.c

--- Assignment_1/prog2.c
+++ Assignment_1/prog2.c
@@ -1,1 +1,23 @@
-//this will be program 2
+// Nicholas Domingue and Bryce Olivier
+#include <stdio.h>
+int my_openit(char* name, int prot);
+void my_closeit(void);
+
+typedef struct funcs{
+    int (*openit)(char *name, int prot);
+    void (*closeit)(void);
+} funcs;
+
+int main(){
+    funcs x = {&my_openit, &my_closeit};
+    return 0;
+}
+
+int my_openit(char *name, int prot){
+    printf("my_openit");
+    return 0;
+}
+
+void my_closeit(void){
+    printf("my_closeit");
+}
