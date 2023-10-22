3a8b90abf25c9f8ff51b1178c8fcb7f36627fc53
Brythe40 <113539993+Brythe40@users.noreply.github.com>
2023-10-22 17:32:25 -0500

    Update mlfq.c

--- Assignment_3/mlfq.c
+++ Assignment_3/mlfq.c
@@ -19,43 +19,57 @@ int num_of_processes = 0;
 int main(int argc, char *argv[]) {
     Queue q1, q2, q3, q4;
     int clock = 0;
-    int element;
+    int element, finished = 0;
 
     // ask user for queue entries
     printf("Queue entries one per line:\n");
     // read input and add to processes array
-    while (num_of_processes <= 10){ // need to come back and make while loop dynamic
+    while (num_of_processes <= 10){ /* need to come back and make while loop dynamic */
         scanf("%d %d %d %d %d", &processes[num_of_processes].times, &processes[num_of_processes].pid, 
                 &processes[num_of_processes].run, &processes[num_of_processes].io, &processes[num_of_processes].repeat);
         num_of_processes++;
     }
-    // calculate length of processes
-    int length = 100 + (num_of_processes - (sizeof(processes) / sizeof(processes[0])));
-
 
     // initializing queue
     init_queue(&q1, sizeof(processes), TRUE, process_compare, FALSE);
 
-    // run while there's another (RUN, I/O, Repeat) phase for process PID
-    while( /* there's another (RUN, I/O, REPEAT) phase for process PID */){
+    // run while there is at least one undetermined process
+    while(finished != num_of_processes){
 
-        for (int i = 0; i < length; i++){
-            // check if process should enter queue
+        for (int i = 0; i < num_of_processes; i++){
+            // check if process should enter queue at current clock value
             if(processes[i].time == clock) {
                 //add process to queue
-                add_to_queue(&q1, &processes[i].pid, current_priority(&q1)); // unsure if this is how to get priority
+                printf("PID: %d, ARRIVAL TIME: %d", processes[i].pid, processes[i].time);
+                add_to_queue(&q1, &processes[i].pid, current_priority(&q1)); /*unsure if this is how to get priority*/
+                printf("CREATE: Process %d entered the ready queue at time %d", processToEnqueue.pid, processToEnqueue.time);
             }
             
             // execute highest-priority process
             element = 0;
             rewind_queue(&q1);
-            if(peek_at_current(&q1) == process[i]){
-                printf("PID: %d, ARRIVAL TIME: %d", processes[i].pid, processes[i].time);
-                printf("CREATE: Process %d entered the ready queue at time %d", processToEnqueue.pid, processToEnqueue.time);
-                
-            }
             // make exit, I/O, demotion or promotion decisions
-            // ...
+            while(/*there's another (RUN, I/O, REPEAT) phase for process PID*/){
+                int i = 0
+                for(i = 0; i < processes[i].repeat; i++){
+                    // do RUN for processes[i].time units
+                    printf("RUN: Process %d started execution from level %d at time %d; wants to execute for %d ticks.", 
+                            processes[i].pid, 1, clock, processes[i].run);
+                    clock += processes[i].run;
+                    // do I/O for processes[i].io time units
+                    printf("I/O: Process %d blocked for I/O at time %d.", processes[i].pid, clock);
+                    clock += processes[i].io;
+                }
+                if(i == processes[i].repeat){
+                    // do RUN for processes[i].time units
+                    printf("RUN: Process %d started execution from level %d at time %d; wants to execute for %d ticks.", 
+                            processes[i].pid, 1, clock, processes[i].run);
+                    clock += processes[i].run;
+                    // do FINISH after RUN time
+                    printf("FINISHED: Process %d finished at time %d.", processes[i].pid, clock);
+                    clock -= 1;
+                }
+            }
          }   
 
         clock++;
@@ -71,7 +85,7 @@ int process_compare(const void *e1, const void *e2) {
   Process *p1=(Process *)e1;
   Process *p2=(Process *)e2;
   // need to compare CPU instead of arrival time
-  if (p1->pid == p2->pid && !p1->time == p2->time) { // change this
+  if (p1->pid == p2->pid && !p1->time == p2->time) { /*change this*/
     return 1;
   }
   else {
