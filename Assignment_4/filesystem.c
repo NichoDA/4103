a33e8c7db02644383a832cc4508f8fb97bbaa0f6
Brythe40 <113539993+Brythe40@users.noreply.github.com>
2023-11-27 19:11:05 -0600

    little progress

--- Assignment_4/filesystem.c
+++ Assignment_4/filesystem.c
@@ -1,1 +1,23 @@
 // Nicholas Domingue and Bryce Olivier
+#include <stdio.h>
+#include <stdlib.h>
+#include "filesystem.h"
+
+typedef struct superblock {
+    int num_inodes;
+} superblock;
+
+typedef struct inode {
+    int size;
+    char name[8];
+} inode;
+
+void print_fs(){
+    printf("Superblock info:\n");
+    printf("\tnum inodes%d\n", s.num_inodes);
+    printf("inodes:\n");
+    int i;
+    for (i = 0; i < sb.num_inodes; i++) {
+        printf("\tsize: %d name: %s", inodes[i].size, indoes[i].name);
+    }
+}
--- Assignment_4/formatfs.c
+++ Assignment_4/formatfs.c
@@ -1,1 +1,18 @@
 // Nicholas Domingue and Bryce Olivier
+#include <stdio.h>
+#include <stdlib.h>
+#include "filesystem.h"
+
+struct superblock sb;
+
+// initialize filesystem
+void init_filesystem() {
+    sb.num_inodes = 10;
+
+    int i;
+    inodes = malloc(sizeof (struct inode) * sb.num_inodes);
+    for(i = 0; i < sb.num_inodes; i++){
+        inodes[i].size = -1;
+        inodes[i].name = "";
+    }
+}
