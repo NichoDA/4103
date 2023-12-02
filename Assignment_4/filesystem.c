62817fb490836f1b98478a00f5c45bcbb932a1a5
Brythe40 <113539993+Brythe40@users.noreply.github.com>
2023-12-02 14:24:13 -0600

    .

--- Assignment_4/filesystem.c
+++ Assignment_4/filesystem.c
@@ -3,21 +3,21 @@
 #include <stdlib.h>
 #include "filesystem.h"
 
-typedef struct superblock {
-    int num_inodes;
-} superblock;
+#define MAX_FILES                     512
+#define DATA_BITMAP_BLOCK               0
+#define INODE_BITMAP_BLOCK              1
+#define FIRST_INODE_BLOCK               2
+#define LAST_INODE_BLOCK                5
 
-typedef struct inode {
-    int size;
-    char name[8];
-} inode;
+#define INODES_PER_BLOCK              128
+#define FIRST_DIR_ENTRY_BLOCK           6
+#define LAST_DIR_ENTRY_BLOCK           69
+#define DIR_ENTRIES_PER_BLOCK           8
 
-void print_fs(){
-    printf("Superblock info:\n");
-    printf("\tnum inodes%d\n", s.num_inodes);
-    printf("inodes:\n");
-    int i;
-    for (i = 0; i < sb.num_inodes; i++) {
-        printf("\tsize: %d name: %s", inodes[i].size, indoes[i].name);
-    }
-}
+#define FIRST_DATA_BLOCK               70
+#define LAST_DATA_BLOCK              4095
+#define MAX_FILENAME_SIZE             507
+#define NUM_DIRECT_INODE_BLOCKS        13
+#define NUM_SINGLE_INDIRECT_BLOCKS      (SOFTWARE_DISK_BLOCK_SIZE / sizeof(unit16_t))
+
+#define MAX_FILE_SIZE                   (NUM_DIRECT_INODE_BLOCKS + NUM_SINGLE_INDIRECT_BLOCKS) * SOFTWARE_DISK_BLOCK_SIZE
--- Assignment_4/formatfs.c
+++ Assignment_4/formatfs.c
@@ -3,16 +3,13 @@
 #include <stdlib.h>
 #include "filesystem.h"
 
-struct superblock sb;
 
 // initialize filesystem
 void init_filesystem() {
-    sb.num_inodes = 10;
+    init_software_disk();
+}
 
-    int i;
-    inodes = malloc(sizeof (struct inode) * sb.num_inodes);
-    for(i = 0; i < sb.num_inodes; i++){
-        inodes[i].size = -1;
-        inodes[i].name = "";
-    }
+
+create_file(char *name){
+
 }
